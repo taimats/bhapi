de1f1fca551254c52bf756239be707db500e5466
taimats <beo8939@gmail.com>
2025-05-10 16:38:17 +0900

    デプロイ時のスクリプトを追加修正

--- scripts/upmig.sh
+++ scripts/upmig.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-source ../.env
-migrate -path ../infra/migrations -database "${PSQL_DSN}" -verbose up
+source ./.env
+migrate -path ./infra/migrations -database "${PSQL_DSN}" -verbose up
