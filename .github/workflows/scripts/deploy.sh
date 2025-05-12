ff3bef3db43ee26e82305fc87e97e61abb734b02
taimats <beo8939@gmail.com>
2025-05-12 12:03:15 +0900

    デプロイ処理の変更

---
+++ .github/workflows/scripts/deploy.sh
@@ -0,0 +1,6 @@
+cd ${{ secrets.SERVER_DEPLOY_DIR }}
+git pull origin main
+go clean -cache && go build -o ${{ secrets.SERVER_EXECUTE_PATH }} -trimpath -ldflags "-w -s"
+chmod +x ${{ secrets.SERVER_EXECUTE_PATH }}
+nohup ${{ secrets.SERVER_EXECUTE_PATH }} &
+mv ./nohup.out ${HOME}/
