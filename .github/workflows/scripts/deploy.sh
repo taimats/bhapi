2ccd2495db61d86dcb66a0359ea2e72f7cc20572
taimats <beo8939@gmail.com>
2025-05-12 13:48:00 +0900

    処理追加

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -1,5 +1,6 @@
 cd ${{ secrets.SERVER_DEPLOY_DIR }}
 git pull origin main
+go get . && go mod tidy
 go clean -cache && go build -o ${{ secrets.SERVER_EXECUTE_PATH }} -trimpath -ldflags "-w -s"
 chmod +x ${{ secrets.SERVER_EXECUTE_PATH }}
 nohup ${{ secrets.SERVER_EXECUTE_PATH }} &
