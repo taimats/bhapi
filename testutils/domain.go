22a384a93e0410af4eeab39b4fc69d1d41063d89
taimats <beo8939@gmail.com>
2025-05-16 01:48:57 +0900

    不要な関数削除

--- testutils/domain.go
+++ testutils/domain.go
@@ -3,35 +3,22 @@ package testutils
 import (
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 )
-
-func GenerateTestJSON(t *testing.T) []byte {
-	t.Helper()
-
-	targetPath := "C:/Users/beo03/bookhistoryapi/domain/search_test/sample2.json"
-	jb, err := os.ReadFile(targetPath)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return jb
-}
 
 func PseudoGoogleBooksAPIServer(t *testing.T) *httptest.Server {
 	t.Helper()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /books/v1/volumes", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		jb := GenerateTestJSON(t)
-
+		testData, err := TestFile("response_body.json")
+		if err != nil {
+			t.Fatal(err)
+		}
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write(jb)
+		w.Write(testData)
 	}))
 
-	ts := httptest.NewServer(mux)
-
-	return ts
+	return httptest.NewServer(mux)
 }
