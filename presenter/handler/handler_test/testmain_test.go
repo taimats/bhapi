55624aacbe1142137a347773bf2323564f45eed5
taimats <beo8939@gmail.com>
2025-05-16 01:52:11 +0900

    呼び出し名変更

--- presenter/handler/handler_test/testmain_test.go
+++ presenter/handler/handler_test/testmain_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestMain(m *testing.M) {
 	//テスト用に環境変数の設定
-	if err := testutils.SetEnvForTest(); err != nil {
+	if err := testutils.DotEnv(); err != nil {
 		log.Fatalf("環境変数の設定に失敗:%v", err)
 	}
 
