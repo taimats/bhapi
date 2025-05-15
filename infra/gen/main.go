675c58ddac75cef33f807771c8b03e3aa39921ee
taimats <beo8939@gmail.com>
2025-05-16 01:44:33 +0900

    ファイル場所＆環境変数の呼び出しの変更

---
+++ infra/gen/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/taimats/bhapi/domain"
+	"github.com/taimats/bhapi/infra"
+	"github.com/taimats/bhapi/testutils"
+	"github.com/uptrace/bun"
+)
+
+func main() {
+	err := testutils.DotEnv()
+	if err != nil {
+		log.Fatalf(".envファイルの取得に失敗:%s", err)
+	}
+
+	conf := infra.NewDBConfig()
+	db, err := infra.NewDatabaseConnection(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	models := []interface{}{
+		(*domain.User)(nil),
+		(*domain.Book)(nil),
+		(*domain.Chart)(nil),
+	}
+
+	var data []byte
+	data = append(data, modelsToByte(db, models)...)
+
+	if err = os.WriteFile("./infra/gen/schema.sql", data, 0777); err == nil {
+		log.Println("DBスキーマファイルを生成")
+	}
+}
+
+func modelsToByte(db *bun.DB, models []interface{}) []byte {
+	var data []byte
+
+	for _, model := range models {
+		query := db.NewCreateTable().Model(model).WithForeignKeys()
+
+		rawQuery, err := query.AppendQuery(db.Formatter(), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data = append(data, rawQuery...)
+		data = append(data, ";\n"...)
+	}
+
+	return data
+}
