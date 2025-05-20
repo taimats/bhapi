5e92659b78d357eda4c6cd89866d80953f30a7b9
taimats <beo8939@gmail.com>
2025-05-20 18:00:14 +0900

    goldenファイルの追加

--- domain/search_test.go
+++ domain/search_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/sebdah/goldie/v2"
+	"github.com/stretchr/testify/assert"
 	"github.com/taimats/bhapi/domain"
 	"github.com/taimats/bhapi/testutils"
 )
@@ -16,50 +18,6 @@ func TestSearchForGoogleBooks(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := []*domain.BookResult{
-		{
-			ISBN10:   "4167110121",
-			ImageURL: "http://books.google.com/books/content?id=TL3APAAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-			Title:    "容疑者Xの献身",
-			Author:   "東野圭吾",
-			Page:     "0",
-			Price:    "0",
-		},
-		{
-			ISBN10:   "",
-			ImageURL: "http://books.google.com/books/content?id=eNjdDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-			Title:    "容疑者Xの献身",
-			Author:   "東野圭吾",
-			Page:     "234",
-			Price:    "770",
-		},
-		{
-			ISBN10:   "",
-			ImageURL: "http://books.google.com/books/content?id=hQDeDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-			Title:    "容疑者Xの献身　無料試し読み版",
-			Author:   "東野圭吾",
-			Page:     "49",
-			Price:    "0",
-		},
-		{
-			ISBN10:   "416711013X",
-			ImageURL: "http://books.google.com/books/content?id=1LcsAwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-			Title:    "ガリレオの苦悩",
-			Author:   "東野圭吾",
-			Page:     "0",
-			Price:    "0",
-		},
-		{
-			ISBN10:   "4167110083",
-			ImageURL: "http://books.google.com/books/content?id=xdM9ywAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-			Title:    "予知夢",
-			Author:   "東野圭吾",
-			Page:     "0",
-			Price:    "0",
-		},
-	}
-	q := "容疑者の献身"
-
 	ts := testutils.PseudoGoogleBooksAPIServer(t)
 	defer ts.Close()
 
@@ -68,17 +26,19 @@ func TestSearchForGoogleBooks(t *testing.T) {
 		t.Fatalf("GoogleBooksAPIテストサーバーでurlパースに失敗:%v", err)
 	}
 	testURL := u.JoinPath("books", "v1", "volumes").String()
+	q := "容疑者の献身"
+
+	a := assert.New(t)
+	g := goldie.New(t, goldie.WithDiffEngine(goldie.ColoredDiff))
 
 	//Act
-	got, err := domain.SearchForGoogleBooks(q, testURL)
-	if err != nil {
-		t.Fatal(err)
-	}
+	results, err := domain.SearchForGoogleBooks(q, testURL)
 
 	//Assert
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Errorf("a mismatch between (-want and +got):%s", diff)
-	}
+	j := testutils.ConvertToJSON(t, results)
+	got := testutils.IndentForJSON(t, j.String())
+	a.Nil(err)
+	g.Assert(t, t.Name(), got)
 }
 
 func TestExtractBooksFromJSON(t *testing.T) {
