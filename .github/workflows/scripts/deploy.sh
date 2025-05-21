7fdee309c3a2ea1c9a1dba647f3eef340f3aad41
taimats <beo8939@gmail.com>
2025-05-22 07:46:38 +0900

    workflowの変更

--- .github/workflows/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-cd ${{ secrets.SERVER_DEPLOY_DIR }}
-git pull origin main
-go get . && go mod tidy
-go clean -cache && go build -o ${{ secrets.SERVER_EXECUTE_PATH }} -trimpath -ldflags "-w -s"
-chmod +x ${{ secrets.SERVER_EXECUTE_PATH }}
-nohup ${{ secrets.SERVER_EXECUTE_PATH }} & > "${HOME}/app_$(date '+%Y_%m_%d_%H:%M:%S').log"
