e08c115f8744a11d68ca3e9e9345325bd1bbd197
taimats <beo8939@gmail.com>
2025-05-23 18:38:02 +0900

    独自エラーを出力する関数を追加更

--- utils/errors.go
+++ utils/errors.go
@@ -2,15 +2,20 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 )
 
-var ErrNotFound = errors.New("リソースがありません")
-var ErrAlrExists = errors.New("すでに存在します")
+var (
+	ErrNotFound  = errors.New("リソースがありません")
+	ErrAlrExists = errors.New("リソースはすでに存在します")
+)
 
-func NewErrNotFound() error {
-	return ErrNotFound
-}
-
-func NewErrAlrExists() error {
-	return ErrNotFound
+// エラー元（＝ origin）を最新エラー（= errNow）でwrapする。
+// errNowには現時点で発生したerrorを入れる。originにはラップしたい元errorを入れる。
+// ラップする必要がない場合、originはnilにしてよい。
+func NewErrChains(errNow error, origin error) error {
+	if origin == nil {
+		return errNow
+	}
+	return fmt.Errorf("%w:%w", errNow, origin)
 }
